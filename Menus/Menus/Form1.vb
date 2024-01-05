19dfaa723cbd28cf997e439e599615e81f3558a9
Subhanik B <buntubix@gmail.com>
2024-01-05 12:59:35 -0700

    Buttons
    Clicking on buttons changes color of background

--- Menus/Menus/Form1.vb
+++ Menus/Menus/Form1.vb
@@ -1,3 +1,8 @@
 ﻿Public Class Form1
-
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.BackColor = Color.Green
+    End Sub
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.BackColor = Color.Red
+    End Sub
 End Class
