9a3c6dce4a5c92565eb96e0218da3eb22d872c3f
Subhanik B <buntubix@gmail.com>
2024-04-08 12:16:18 -0700

    Completely Unneccesary
    But it made it better

--- DroneDad/DroneDad/Form1.vb
+++ DroneDad/DroneDad/Form1.vb
@@ -40,87 +40,23 @@
         If (DroneVendingMachine16.count > 0) Then TextBox1.Text += DroneVendingMachine16.label + ": " + DroneVendingMachine16.count.ToString + "
 "
         TextBox1.Text += "All for a grand total of: " + cost.ToString("C2")
-        '        If (DroneVendingMachine1.count > 0) Then
-        '            TextBox1.Visible = True
-        '            TextBox1.Text = " Thank you for your purchase! You have bought " + DroneVendingMachine1.count.ToString() + " " + DroneVendingMachine1.label
-        '        End If
-        '        If (DroneVendingMachine2.count > 0) Then
-        '            TextBox1.Visible = True
-        '            TextBox1.Text += "
-        'Thank you for your purchase! You have bought " + DroneVendingMachine2.count.ToString() + " " + DroneVendingMachine2.label
-        '        End If
-        '        If (DroneVendingMachine3.count > 0) Then
-        '            TextBox1.Visible = True
-        '            TextBox1.Text += "
-        'Thank you for your purchase! You have bought " + DroneVendingMachine3.count.ToString() + " " + DroneVendingMachine3.label
-        '        End If
-        '        If (DroneVendingMachine4.count > 0) Then
-        '            TextBox1.Visible = True
-        '            TextBox1.Text += "
-        'Thank you for your purchase! You have bought " + DroneVendingMachine4.count.ToString() + " " + DroneVendingMachine4.label
-        '        End If
-        '        If (DroneVendingMachine5.count > 0) Then
-        '            TextBox1.Visible = True
-        '            TextBox1.Text += "
-        'Thank you for your purchase! You have bought " + DroneVendingMachine5.count.ToString() + " " + DroneVendingMachine5.label
-        '        End If
-        '        If (DroneVendingMachine6.count > 0) Then
-        '            TextBox1.Visible = True
-        '            TextBox1.Text += "
-        'Thank you for your purchase! You have bought " + DroneVendingMachine6.count.ToString() + " " + DroneVendingMachine6.label
-        '        End If
-        '        If (DroneVendingMachine7.count > 0) Then
-        '            TextBox1.Visible = True
-        '            TextBox1.Text += "
-        'Thank you for your purchase! You have bought " + DroneVendingMachine7.count.ToString() + " " + DroneVendingMachine7.label
-        '        End If
-        '        If (DroneVendingMachine8.count > 0) Then
-        '            TextBox1.Visible = True
-        '            TextBox1.Text += "
-        'Thank you for your purchase! You have bought " + DroneVendingMachine8.count.ToString() + " " + DroneVendingMachine8.label
-        '        End If
-        '        If (DroneVendingMachine9.count > 0) Then
-        '            TextBox1.Visible = True
-        '            TextBox1.Text += "
-        'Thank you for your purchase! You have bought " + DroneVendingMachine9.count.ToString() + " " + DroneVendingMachine9.label
-        '        End If
-        '        If (DroneVendingMachine10.count > 0) Then
-        '            TextBox1.Visible = True
-        '            TextBox1.Text += "
-        'Thank you for your purchase! You have bought " + DroneVendingMachine10.count.ToString() + " " + DroneVendingMachine10.label
-        '        End If
-        '        If (DroneVendingMachine11.count > 0) Then
-        '            TextBox1.Visible = True
-        '            TextBox1.Text += "
-        'Thank you for your purchase! You have bought " + DroneVendingMachine11.count.ToString() + " " + DroneVendingMachine11.label
-        '        End If
-        '        If (DroneVendingMachine12.count > 0) Then
-        '            TextBox1.Visible = True
-        '            TextBox1.Text += "
-        'Thank you for your purchase! You have bought " + DroneVendingMachine12.count.ToString() + " " + DroneVendingMachine12.label
-        '        End If
-        '        If (DroneVendingMachine13.count > 0) Then
-        '            TextBox1.Visible = True
-        '            TextBox1.Text += "
-        'Thank you for your purchase! You have bought " + DroneVendingMachine13.count.ToString() + " " + DroneVendingMachine13.label
-        '        End If
-        '        If (DroneVendingMachine14.count > 0) Then
-        '            TextBox1.Visible = True
-        '            TextBox1.Text += "
-        'Thank you for your purchase! You have bought " + DroneVendingMachine14.count.ToString() + " " + DroneVendingMachine14.label
-        '        End If
-        '        If (DroneVendingMachine15.count > 0) Then
-        '            TextBox1.Visible = True
-        '            TextBox1.Text += "
-        'Thank you for your purchase! You have bought " + DroneVendingMachine15.count.ToString() + " " + DroneVendingMachine15.label
-        '        End If
-        '        If (DroneVendingMachine16.count > 0) Then
-        '            TextBox1.Visible = True
-        '            TextBox1.Text += "
-        'Thank you for your purchase! You have bought " + DroneVendingMachine16.count.ToString() + " " + DroneVendingMachine16.label
-        '        End If
-        '        TextBox1.Text += "
-        'All for a grand total of " + ShoppingCart.Text
+        cost = 0
+        DroneVendingMachine1.count = 0
+        DroneVendingMachine2.count = 0
+        DroneVendingMachine3.count = 0
+        DroneVendingMachine4.count = 0
+        DroneVendingMachine5.count = 0
+        DroneVendingMachine6.count = 0
+        DroneVendingMachine7.count = 0
+        DroneVendingMachine8.count = 0
+        DroneVendingMachine9.count = 0
+        DroneVendingMachine10.count = 0
+        DroneVendingMachine11.count = 0
+        DroneVendingMachine12.count = 0
+        DroneVendingMachine13.count = 0
+        DroneVendingMachine14.count = 0
+        DroneVendingMachine15.count = 0
+        DroneVendingMachine16.count = 0
     End Sub
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
         DroneVendingMachine1.Visible = False
