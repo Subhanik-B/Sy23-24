f197689e5a2ab0bfbb56060b2371748305584456
Subhanik B <buntubix@gmail.com>
2024-04-26 11:50:47 -0700

    pictore work

--- Astro/Astro/MainWindow.xaml.vb
+++ Astro/Astro/MainWindow.xaml.vb
@@ -6,51 +6,51 @@
         d = Calendar.SelectedDate.Value.Day
         If (m = 1 And d >= 20 Or m = 2 And d <= 18) Then
             Display.Content = "Aquarius"
-            Aquarius.Visibility = IsVisible
+            Aquarius.Visibility = Visibility.Visible
         End If
         If (m = 2 And d >= 19 Or m = 3 And d <= 20) Then
             Display.Content = "Pisces"
-            Pisces.Visibility = IsVisible
+            Pisces.Visibility = Visibility.Visible
         End If
         If (m = 3 And d >= 21 Or m = 4 And d <= 19) Then
             Display.Content = "Aries"
-            Aries.Visibility = IsVisible
+            Aries.Visibility = Visibility.Visible
         End If
         If (m = 4 And d >= 20 Or m = 5 And d <= 20) Then
             Display.Content = "Taurus"
-            Taurus.Visibility = IsVisible
+            Taurus.Visibility = Visibility.Visible
         End If
         If (m = 5 And d >= 21 Or m = 6 And d <= 21) Then
             Display.Content = "Gemini"
-            Gemini.Visibility = IsVisible
+            Gemini.Visibility = Visibility.Visible
         End If
         If (m = 6 And d >= 22 Or m = 7 And d <= 22) Then
             Display.Content = "Cancer"
-            Cancer.Visibility = IsVisible
+            Cancer.Visibility = Visibility.Visible
         End If
         If (m = 7 And d >= 23 Or m = 8 And d <= 22) Then
             Display.Content = "Leo"
-            Leo.Visibility = IsVisible
+            Leo.Visibility = Visibility.Visible
         End If
         If (m = 8 And d >= 23 Or m = 9 And d <= 22) Then
             Display.Content = "Virgo"
-            Virgo.Visibility = IsVisible
+            Virgo.Visibility = Visibility.Visible
         End If
         If (m = 9 And d >= 23 Or m = 10 And d <= 22) Then
             Display.Content = "Libra"
-            Libra.Visibility = IsVisible
+            Libra.Visibility = Visibility.Visible
         End If
         If (m = 10 And d >= 23 Or m = 11 And d <= 21) Then
             Display.Content = "Scorpio"
-            Scorpio.Visibility = IsVisible
+            Scorpio.Visibility = Visibility.Visible
         End If
         If (m = 11 And d >= 22 Or m = 12 And d <= 21) Then
             Display.Content = "Sagittarius"
-            Sagittarius.Visibility = IsVisible
+            Sagittarius.Visibility = Visibility.Visible
         End If '
         If (m = 12 And d >= 22 Or m = 1 And d <= 19) Then
             Display.Content = "Capricorn"
-            Capricorn.Visibility = IsVisible
+            Capricorn.Visibility = Visibility.Visible
         End If
         'Display.Content = Calendar.SelectedDate.Value
     End Sub
